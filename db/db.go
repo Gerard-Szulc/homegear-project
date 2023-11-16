446ee516826c759e120ae2991575f4ba995f983f
Gerard Szulc <gerszulc05@gmail.com>
2023-11-17 00:56:16 +0100

    feat(db): add postgres

--- db/db.go
+++ db/db.go
@@ -2,7 +2,7 @@ package db
 
 import (
 	"fmt"
-	"gorm.io/driver/sqlite"
+	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 	"log"
 	"os"
@@ -11,35 +11,28 @@ import (
 var DB *gorm.DB
 
 func InitiateDB() {
-
-	// sqlite for testing purposes
-	sqliteFilePath, exists := os.LookupEnv("SQLITE_DB_DUST_PATH")
+	dbport, exists := os.LookupEnv("DBPORT")
 	if !exists {
-		fmt.Println("sqlite env doesnt exist")
+		fmt.Println(exists)
 	}
-
-	//dbport, exists := os.LookupEnv("DBPORT")
-	//if !exists {
-	//	fmt.Println(exists)
-	//}
-	//user, exists := os.LookupEnv("DBUSER")
-	//if !exists {
-	//	fmt.Println(exists)
-	//}
-	//password, exists := os.LookupEnv("DBPASSWORD")
-	//if !exists {
-	//	fmt.Println(exists)
-	//}
-	//dbname, exists := os.LookupEnv("DBNAME")
-	//if !exists {
-	//	fmt.Println(exists)
-	//}
-	db, err := gorm.Open(sqlite.Open(sqliteFilePath), &gorm.Config{})
+	user, exists := os.LookupEnv("DBUSER")
+	if !exists {
+		fmt.Println(exists)
+	}
+	password, exists := os.LookupEnv("DBPASSWORD")
+	if !exists {
+		fmt.Println(exists)
+	}
+	dbname, exists := os.LookupEnv("DBNAME")
+	if !exists {
+		fmt.Println(exists)
+	}
+	dsn := fmt.Sprintf("host=localhost port=%s user=%s password=%s dbname=%s", dbport, user, password, dbname)
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatal(err)
 	}
 	//db.DB().SetMaxIdleConns(20)
 	//db.DB().SetMaxOpenConns(200)
 	DB = db
-
 }
